45f4d94e595bb048caac36087de23273f8284bb0
cvcosta96 <carloscosta24@live.com.pt>
2017-06-23 23:23:55 +0100

    nada feito

--- Classes/build/generated-sources/ap-source-output/classes/Cliente_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Cliente_.java
@@ -9,11 +9,9 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
+
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T19:28:52")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T21:05:45")
->>>>>>> a6d71efda95411a0de9573c466ecd76310401a1a
+
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
