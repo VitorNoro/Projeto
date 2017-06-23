d0316dfe7fdeb6e7371d8ec4583233de6fae0483
cvcosta96 <carloscosta24@live.com.pt>
2017-06-23 14:06:12 +0100

    123

--- Classes/build/generated-sources/ap-source-output/classes/Venda_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Venda_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T12:09:03")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T13:59:35")
 @StaticMetamodel(Venda.class)
 public class Venda_ { 
 
