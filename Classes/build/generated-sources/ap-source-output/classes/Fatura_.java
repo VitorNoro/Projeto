4caaffc05f78a8ee63fe8440a1177a417cfd36e9
VitorNoro <vmnoro@gmail.com>
2017-06-24 21:01:05 +0100

    gen src

--- Classes/build/generated-sources/ap-source-output/classes/Fatura_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Fatura_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T21:05:46")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T23:11:35")
 @StaticMetamodel(Fatura.class)
 public class Fatura_ { 
 
--- Classes/build/generated-sources/ap-source-output/classes/Reparacao_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Reparacao_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T21:05:45")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-23T23:11:35")
 @StaticMetamodel(Reparacao.class)
 public class Reparacao_ { 
 
