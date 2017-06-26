6c099d1168aef3d84d684ba8a137c2e2247d29bc
cvcosta96 <carloscosta24@live.com.pt>
2017-06-26 16:17:15 +0100

    123

--- Classes/build/generated-sources/ap-source-output/classes/Pagamento_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Pagamento_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-26T15:30:42")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-26T15:50:48")
 @StaticMetamodel(Pagamento.class)
 public class Pagamento_ { 
 
