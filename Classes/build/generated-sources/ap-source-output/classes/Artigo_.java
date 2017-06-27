2aa53c21b60295ec2a5843ecc191b8a52b1a55bc
cvcosta96 <carloscosta24@live.com.pt>
2017-06-27 01:29:39 +0100

    mais ecrãs
    falta a parte do caixa, os que fiz temos de ver se queres assim também...

--- Classes/build/generated-sources/ap-source-output/classes/Artigo_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Artigo_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-26T22:03:27")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-26T23:44:30")
 @StaticMetamodel(Artigo.class)
 public class Artigo_ { 
 
