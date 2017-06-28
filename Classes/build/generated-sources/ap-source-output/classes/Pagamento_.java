91f5c5da46f67cc4529c3f43fd4c3ece75824fa8
VitorNoro <vmnoro@gmail.com>
2017-06-28 22:55:30 +0100

    dsf

--- Classes/build/generated-sources/ap-source-output/classes/Pagamento_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Pagamento_.java
@@ -7,17 +7,9 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
+
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T22:47:04")
-=======
 
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T22:45:39")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T22:34:13")
-
->>>>>>> cd954642b5297bdd1d7e90d60678656e829cadce
->>>>>>> 81e3f438fc30b957a2fe1184bf17c662f349496d
 @StaticMetamodel(Pagamento.class)
 public class Pagamento_ { 
 
--- Classes/build/generated-sources/ap-source-output/classes/Reparacao_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Reparacao_.java
@@ -8,17 +8,8 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-<<<<<<< HEAD
+
 @Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T22:47:04")
-=======
-
-<<<<<<< HEAD
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T22:45:39")
-=======
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T22:34:13")
-
->>>>>>> cd954642b5297bdd1d7e90d60678656e829cadce
->>>>>>> 81e3f438fc30b957a2fe1184bf17c662f349496d
 @StaticMetamodel(Reparacao.class)
 public class Reparacao_ { 
 
