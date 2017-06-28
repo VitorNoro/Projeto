34fb57fcff66e8bcd6a7bf04321f329edf6b9fe7
cvcosta96 <carloscosta24@live.com.pt>
2017-06-28 16:01:58 +0100

    fundo mais claro em todos os ecrãs

--- Classes/build/generated-sources/ap-source-output/classes/LinhaArtigo_.java
+++ Classes/build/generated-sources/ap-source-output/classes/LinhaArtigo_.java
@@ -6,7 +6,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T15:48:07")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T16:00:41")
 @StaticMetamodel(LinhaArtigo.class)
 public class LinhaArtigo_ { 
 
