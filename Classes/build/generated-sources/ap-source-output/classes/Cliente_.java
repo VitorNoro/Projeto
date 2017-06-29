5d5bb992ce45b82bd95b442b7c47d29b75418359
cvcosta96 <carloscosta24@live.com.pt>
2017-06-29 17:54:07 +0100

    123

--- Classes/build/generated-sources/ap-source-output/classes/Cliente_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Cliente_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T23:54:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-29T11:51:11")
 @StaticMetamodel(Cliente.class)
 public class Cliente_ { 
 
--- Classes/build/generated-sources/ap-source-output/classes/Fatura_.java
+++ Classes/build/generated-sources/ap-source-output/classes/Fatura_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-28T23:54:37")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-06-29T11:51:11")
 @StaticMetamodel(Fatura.class)
 public class Fatura_ { 
 
